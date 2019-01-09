50aa76882fd003c6674cbe5f25a5da8c2c0df937
kbell <k.bell@cmcmarkets.com>
2019-01-09 09:54:08 +0000

    oioi
    Signed-off-by: kbell <k.bell@cmcmarkets.com>

--- src/AniLookup.java
+++ src/AniLookup.java
@@ -1,4 +1,50 @@
+
 import java.util.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 //this will be a lookup evaluating current lists of released tv and recoommending
 //them in order of what genres you have selected. ui will be in html page
+
+class AniLookup{
+
+
+    public static void main(String []args){
+        HashSet<String> anilist = new HashSet<String>();
+
+        anilist.add("starter");
+        Array newP = collectFromOnline();
+
+
+    }
+
+    public static Array[] collectFromOnline(){
+        System.setProperty("webdriver.chrome.driver", "G:\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        String baseUrl = "web site with list";
+        String expectedTitle = "welcome...";
+        String actualTitle = "";
+
+        driver.get(baseUrl);
+
+        actualTitle = driver.getTitle();
+
+        if (actualTitle.contentEquals(expectedTitle)){
+            System.out.println("Test Passed");
+        } else {
+            System.out.println("test failed: " + actualTitle);
+        }
+        driver.close();
+        Array[] n = new Array[1];
+        return n;
+
+    }
+    
+    public void manAdd(HashSet<String> hs, String addThis){
+        hs.add(addThis);
+    }
+    
+
+
+}
